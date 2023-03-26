fa0462cd1dac86483a09b8fca7002f883824903a
Gabries <105074547+gabrieszin@users.noreply.github.com>
2023-03-26 20:02:20 -0300

    Criando class de conexão

---
+++ dao/conexao.php
@@ -0,0 +1,31 @@
+<?php
+
+class conexao{
+  
+  public function conectar(){
+    $config = array(
+      'host' => 'localhost',
+      'dbname' => 'ponto_eletronico',
+      'charset' => 'utf8mb4',
+      'dbuser' => 'root',
+      'dbpass' => '',
+    );
+
+    try{
+      $pdo = new PDO('mysql:host='. $config['host']. ';dbname='. $config['dbname']. ';charset='. $config['charset'], $config['dbuser'], $config['dbpass']);
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      return $pdo;
+    }catch(Exception $e){
+      print($e);
+      return null;
+    }catch(PDOException $e){
+      print($e);
+      return null;
+    }
+  }
+  
+  public static function fechar($pdo){
+    unset($pdo);
+  }
+}
+?>
