8efe3cdb8bf7a4131fe55874ce007e729f4a53c3
Gabries <105074547+gabrieszin@users.noreply.github.com>
2023-03-26 19:29:44 -0300

    Tratando dados recebidos via POST

--- controller/login-controller.php
+++ controller/login-controller.php
@@ -6,6 +6,29 @@
     if(!empty($_POST['dados']['usuario']) && !empty($_POST['dados']['senha'])){
       $retorno['mensagem'] = 'Dados recebidos';
       $retorno['sucesso'] = true;
+      
+      $filtro = array(
+        "dados" => array(
+          "usuario" => array(
+            "filter" => FILTER_SANITIZE_SPECIAL_CHARS,
+            "flags" => FILTER_FORCE_ARRAY,
+            "options" => "ucwords"
+          ),
+          "senha" => array(
+            "filter" => FILTER_SANITIZE_SPECIAL_CHARS,
+            "flags" => FILTER_FORCE_ARRAY,
+            "options" => "ucwords"
+          )
+        )
+        );
+
+      
+      $usuario = filter_var($_POST['dados']['usuario'], FILTER_SANITIZE_SPECIAL_CHARS, FILTER_NULL_ON_FAILURE);
+      $usuario = trim(strtolower($usuario));
+
+      $senha = filter_var($_POST['dados']['senha'], FILTER_SANITIZE_SPECIAL_CHARS, FILTER_NULL_ON_FAILURE);
+      $senha = trim($senha);
+
     }else{
       $retorno['mensagem'] = 'Dados não recebidos ou incompletos';
       $retorno['sucesso'] = false;
